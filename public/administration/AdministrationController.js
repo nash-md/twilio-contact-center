e76848703c393676c52007a9cf60c53644023f12
nash-md <mdamm@twilio.com>
2016-07-12 13:26:54 +0200

    added directive to handle IVR options as integer

--- public/administration/AdministrationController.js
+++ public/administration/AdministrationController.js
@@ -146,7 +146,7 @@ administrationController.controller('AdministrationController', function ($scope
 
   $scope.createIvrOption = function(){
 
-    var option = { friendlyName: 'unknown', option: 1, taskQueueSid: null };
+    var option = { friendlyName: 'unknown' };
 
     $scope.configuration.ivr.options.push(option);
     $scope.createForm = false;
@@ -202,6 +202,20 @@ administrationController.directive('clientName', function () {
         }
 
       };
+    }
+  };
+});
+
+administrationController.directive('convertToNumber', function() {
+  return {
+    require: 'ngModel',
+    link: function(scope, element, attrs, ngModel) {
+      ngModel.$parsers.push(function(value) {
+        return value ? parseInt(value, 10) : null;
+      });
+      ngModel.$formatters.push(function(value) {
+        return value ? '' + value : null;
+      });
     }
   };
 });
