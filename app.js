163625039d6c3d815770ba56f4c8e08df49cc5d7
nash-md <mdamm@twilio.com>
2016-09-12 22:05:29 +0200

    fixed session expiration

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ var app = express()
 app.set('port', (process.env.PORT || 5000))
 
 app.use(compression())
-app.use(sessions({resave: true, saveUninitialized: false, secret: 'keyboard cat', name: 'session',  cookie: {expires: util.generateSessionExirationDate() }}))
+app.use(sessions({resave: true, saveUninitialized: false, secret: 'keyboard cat', name: 'session',  cookie: {expires: util.generateSessionExirationDate(3600) }}))
 app.use(bodyParser.json({})) 
 app.use(bodyParser.urlencoded({
   extended: true
