12f11f9994b66e89f02092406e3b6566832b23b3
nash-md <mdamm@twilio.com>
2017-06-08 18:47:31 +0100

    eslint cleanup

--- public/IndexController.js
+++ public/IndexController.js
@@ -1,21 +1,20 @@
 var app = angular.module('indexApplication', []);
 
 app.controller('IndexController', function ($scope, $http) {
+	$scope.setup;
+	$scope.code;
 
-  $scope.setup;
-  $scope.code;
+	$scope.validateSetup = function () {
 
-  $scope.validateSetup = function(){
+		$http.post('/api/validate/setup')
+			.then(function onSuccess (response) {
+				$scope.setup = 'VALID';
+				$scope.code = null;
+			}, function onError (response) {
+				$scope.setup = 'INVALID';
+				$scope.code = response.data.code;
+			});
 
-    $http.post('/api/validate/setup')
-      .then(function onSuccess(response) {
-        $scope.setup = 'VALID';
-        $scope.code = null;
-      }, function onError(response) { 
-        $scope.setup = 'INVALID';
-        $scope.code = response.data.code;
-      });
-
-  };
+	};
 
 });
