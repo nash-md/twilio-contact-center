452dcfdd8bd8bc65d2b2575d7c2cf93157002f42
yuto.yamada <yuto.yamada@kddi-web.com>
2017-07-02 12:40:45 +0900

    [add] Separate phone number and SMS number because 050 numbers can not use SMS

--- controllers/setup-phone-number.js
+++ controllers/setup-phone-number.js
@@ -1,19 +1,29 @@
-const Twilio 	= require('twilio')
+const Twilio = require('twilio')
+const async = require('async')
 
 const client = new Twilio(
 	process.env.TWILIO_ACCOUNT_SID,
 	process.env.TWILIO_AUTH_TOKEN)
 
 module.exports.update = function (req, res) {
-	console.log('configure number: ' + req.body.sid)
+	console.log('configure number: ' + req.body.numberSids.callSid)
+	console.log('configure number: ' + req.body.numberSids.smsSid)
+
 	const voiceUrl = (process.env.PROTOCOL || req.protocol) + '://'
 		+ (process.env.DOCKER_HOST_DOMAIN || req.hostname) + '/api/ivr/welcome'
 	const smsUrl = (process.env.PROTOCOL || req.protocol) + '://'
 		+ (process.env.DOCKER_HOST_DOMAIN || req.hostname) + '/api/messaging-adapter/inbound'
 
-	client.incomingPhoneNumbers(req.body.sid).update({
+	client.incomingPhoneNumbers(req.body.numberSids.callSid).update({
 		voiceUrl: voiceUrl,
-		voiceMethod: 'GET',
+		voiceMethod: 'GET'
+	}).then(phoneNumber => {
+		res.status(200).end()
+	}).catch(error => {
+		res.status(500).send(res.convertErrorToJSON(error))
+	})
+
+	client.incomingPhoneNumbers(req.body.numberSids.smsSid).update({
 		smsUrl: smsUrl,
 		smsMethod: 'POST'
 	}).then(phoneNumber => {
@@ -25,34 +35,50 @@ module.exports.update = function (req, res) {
 }
 
 module.exports.validate = function (req, res) {
-	const filter = {
-		phoneNumber: req.body.callerId
-	}
 
-	client.incomingPhoneNumbers.list(filter).then(phoneNumbers => {
-		/* phone number not found */
-		if (phoneNumbers.length === 0) {
-			return res.status(404).json({ code: 'TWILIO_PHONE_NUMBER_NOT_FOUND'})
+	const numberSids = { callSid: null, smsSid: null }
+	const ids = [ req.body.callerId, req.body.smsId ]
+
+	async.eachOf(ids, function (id, idx, callback) {
+
+		const filter = {
+			phoneNumber: id
 		}
 
-		/* the query returned more than one number, something went wrong */
-		if (phoneNumbers.length !== 1) {
-			return res.status(500).json({ code: 'TWILIO_MULTIPLE_PHONE_NUMBERS'})
-		}
+		client.incomingPhoneNumbers.list(filter).then(phoneNumbers => {
+			/* phone number not found */
+			if (phoneNumbers.length === 0) {
+				return res.status(404).json({ code: 'TWILIO_PHONE_NUMBER_NOT_FOUND'})
+			}
 
-		/* the number does not support voice */
-		if (phoneNumbers[0].capabilities.voice === false) {
-			return res.status(500).json({ code: 'TWILIO_PHONE_NUMBER_NOT_VOICE_CAPABLE'})
-		}
+			/* the query returned more than one number, something went wrong */
+			if (phoneNumbers.length !== 1) {
+				return res.status(500).json({ code: 'TWILIO_MULTIPLE_PHONE_NUMBERS'})
+			}
 
-		let sid = phoneNumbers[0].sid
-		let capabilities = phoneNumbers[0].capabilities
+			/* the number does not support voice */
+			if (phoneNumbers[0].capabilities.voice === false) {
+				return res.status(500).json({ code: 'TWILIO_PHONE_NUMBER_NOT_VOICE_CAPABLE'})
+			}
 
-		res.status(200).json({sid: sid, capabilities: capabilities})
-	}).catch(error => {
-		res.status(500).json({
-			code: 'TWILIO_UNKNOWN_ERROR', message: req.util.convertToString(error)
+			let sid = phoneNumbers[0].sid
+
+			if (idx === 0) {
+				numberSids.callSid = sid
+			} else {
+				numberSids.smsSid = sid
+			}
+
+			callback()
+
+		}).catch(error => {
+			res.status(500).json({
+				code: 'TWILIO_UNKNOWN_ERROR', message: req.util.convertToString(error)
+			})
 		})
+
+	}, function () {
+		res.status(200).json({numberSids: numberSids})
 	})
 
 }
