14e24022ccb4149c7a085a9b986bd536380f773f
nash-md <mdamm@twilio.com>
2018-04-20 11:02:53 +0200

    fixed worker status with multiple tabs/windows

--- public/callcenter/WorkflowController.js
+++ public/callcenter/WorkflowController.js
@@ -69,6 +69,7 @@ app.controller('WorkflowController', function ($scope, $rootScope, $http, $inter
 			$log.log('TaskRouter Worker: ready');
 
 			$scope.worker = worker;
+			$scope.$apply();
 
 		});
 
