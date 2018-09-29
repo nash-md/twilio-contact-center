6ec1fdbd59c5d2e336f9c90ffb68ee5529066eaa
nash-md <mdamm@twilio.com>
2018-09-29 12:06:15 +0200

    remove nested promise

--- controllers/helpers/chat-helper.js
+++ controllers/helpers/chat-helper.js
@@ -1,23 +1,12 @@
-const Twilio 	= require('twilio')
+const twilio 	= require('twilio')
 
-const client = new Twilio(
+const client = twilio(
 	process.env.TWILIO_ACCOUNT_SID,
 	process.env.TWILIO_AUTH_TOKEN)
 
 module.exports.createChannel = (name) => {
-	const data = {
+	return client.chat.services(process.env.TWILIO_CHAT_SERVICE_SID).channels.create({
 		friendlyName: 'Support Chat with ' + name,
 		uniqueName: 'support_channel_' + Math.random().toString(36).substring(7)
-	}
-
-	return new Promise((resolve, reject) => {
-		client.chat.services(process.env.TWILIO_CHAT_SERVICE_SID).channels.create(data)
-			.then(channel => {
-				resolve(channel)
-			}).catch(error => {
-				reject(error)
-			})
-
 	})
-
 }
