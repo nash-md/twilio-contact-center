8d3f977e47ec8b388504da99310d83cb2d187172
Matthias Damm <mdamm@twilio.com>
2020-02-14 18:26:30 +0100

    API keys instead of AccountSid/AuthToken#

--- controllers/taskrouter.js
+++ controllers/taskrouter.js
@@ -1,43 +1,38 @@
-const twilio 	= require('twilio')
+const twilio = require('twilio');
 
-const client = twilio(
-	process.env.TWILIO_ACCOUNT_SID,
-	process.env.TWILIO_AUTH_TOKEN)
+const client = twilio(process.env.TWILIO_API_KEY_SID, process.env.TWILIO_API_KEY_SECRET, {
+	accountSid: process.env.TWILIO_ACCOUNT_SID
+});
 
-module.exports.getWorkspace = function (req, res) {
-
-	client.taskrouter.workspaces(process.env.TWILIO_WORKSPACE_SID).fetch()
-		.then(workspace => {
-			let payload = {
+module.exports.getWorkspace = function(req, res) {
+	client.taskrouter
+		.workspaces(process.env.TWILIO_WORKSPACE_SID)
+		.fetch()
+		.then((workspace) => {
+			const payload = {
 				sid: workspace.sid,
 				friendlyName: workspace.friendlyName
-			}
+			};
 
-			res.status(200).json(payload)
-		}).catch(error => {
-			res.status(500).send(res.convertErrorToJSON(error))
+			res.status(200).json(payload);
 		})
+		.catch((error) => {
+			res.status(500).send(res.convertErrorToJSON(error));
+		});
+};
 
-}
+module.exports.getActivities = function(req, res) {
+	client.taskrouter
+		.workspaces(process.env.TWILIO_WORKSPACE_SID)
+		.activities.list()
+		.then((activities) => {
+			const payload = activities.map((activity) => {
+				return { sid: activity.sid, friendlyName: activity.friendlyName };
+			});
 
-module.exports.getActivities = function (req, res) {
-
-	client.taskrouter.workspaces(process.env.TWILIO_WORKSPACE_SID).activities.list()
-		.then(activities => {
-			let payload =[]
-
-			for (let i = 0; i < activities.length; i++) {
-				const activity = {
-					sid: activities[i].sid,
-					friendlyName: activities[i].friendlyName,
-				}
-
-				payload.push(activity)
-			}
-
-			res.status(200).json(payload)
-		}).catch(error => {
-			res.status(500).send(res.convertErrorToJSON(error))
+			res.status(200).json(payload);
 		})
-
-}
+		.catch((error) => {
+			res.status(500).send(res.convertErrorToJSON(error));
+		});
+};
